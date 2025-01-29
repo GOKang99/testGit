210db91c9c569a660908a5d1c7353e3b521422a7
MacOmer <justletomer@gmail.com>
2025-01-30 00:58:43 +0900

    Initial Commit

---
+++ testGit/testGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitApp.swift
+//  testGit
+//
+//  Created by Omer on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct testGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
